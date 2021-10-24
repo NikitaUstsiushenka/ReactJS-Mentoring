55bd282d0173384066e59104503c24bd28723070
Nikita Ustsiushenka <nikita09061999@mail.ru>
2021-10-24 19:30:15 +0300

    Update Counter.js

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,19 +1,18 @@
 import React, { useState } from 'react';
 
 const Counter = () => {
-	const [count, setCount] = useState(0);
-
-	return (
-		<div>
-			<button
-				type="button"
-				onClick={() => setCount(count + 1)}
-			>
-				{'Click'}
-			</button>
-			<p>{`Count of clicks: ${count}`}</p>
-		</div>
-	);
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <button
+	type="button"
+	onClick={() => setCount(count + 1)}
+      >
+        {'Click'}
+      </button>
+      <p>{`Count of clicks: ${count}`}</p>
+    </div>
+  );
 };
 
 export default Counter;
