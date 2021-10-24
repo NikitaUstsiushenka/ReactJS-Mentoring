4094ed2f0d551de9fbf8a844eeb9f2df33f47dbd
Nikita Ustsiushenka <nikita09061999@mail.ru>
2021-10-24 19:27:37 +0300

    Update Search.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,30 +1,29 @@
 import React from 'react';
 
 export default class Search extends React.Component {
-	state = {
-		searchValue: '',
-	}
+  state = {
+    searchValue: '',
+  }
 
-	handleSubmit = () => {
-		alert(this.state.searchValue);
-	}
+  handleSubmit = () => {
+    alert(this.state.searchValue);
+  }
 
-	handleChange = (event) => {
-		this.setState({ searchValue: event.target.value });
-	}
+  handleChange = (event) => {
+    this.setState({ searchValue: event.target.value });
+  }
 
-	render() {
-		const { searchValue } = this.state;
-
-		return (
-			<div>
-				<form onSubmit={this.handleSubmit}>
-					<input value={searchValue} onChange={this.handleChange} />
-					<button type="submit">
-						{'Search'}
-					</button>
-				</form>
-			</div>
-		);
-	}
+  render() {
+    const { searchValue } = this.state;
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+	  <input value={searchValue} onChange={this.handleChange} />
+	  <button type="submit">
+	    {'Search'}
+	  </button>
+        </form>
+      </div>
+    );
+  }
 }
