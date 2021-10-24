2ec0907fcdbf2d50bd8a96f13c712243eaba5e4c
Mikita_Ustsiushenka <v-mustsiushenka@expediagroup.com>
2021-10-24 17:06:00 +0300

    Task-1: Creating different components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Counter from './components/Counter';
+import Search from './components/Search';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Home />
+        <Counter />
+        <Search />
       </header>
     </div>
   );
